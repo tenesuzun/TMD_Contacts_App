c49b51a8dcd5e682035f356a0a5df5a06d02c458
enes.uzun <enes.uzun@sabah.com.tr>
2021-06-22 17:16:03 +0300

    Fragments tests has been started: - activity contact list modifying into fragment transitions has been started. Must continue

--- app/src/main/java/com/example/tmdcontactsapp/ContactsListActivity.kt
+++ app/src/main/java/com/example/tmdcontactsapp/ContactsListActivity.kt
@@ -3,11 +3,35 @@ package com.example.tmdcontactsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.tmdcontactsapp.adapters.ContactListAdapter
 import com.example.tmdcontactsapp.models.DataSource
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_contacts_list.*
 
 class ContactsListActivity : AppCompatActivity() {
+
+    private val navigationItemSelected = BottomNavigationView.OnNavigationItemSelectedListener {item ->
+        when(item.itemId){
+            R.id.contactsPage ->{
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.groupsPage ->{
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.profilePage -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.removeContact -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.addNewContact -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +47,20 @@ class ContactsListActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        initRecyclerView()
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelected)
+
+        //initRecyclerView()
         addDataSet()
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction
+        TODO("This will be the place to manage the fragments transitions")
     }
 
     private fun addDataSet(){
         val data = DataSource.createContactsList()
-        contactsAdapter.submitList(data)
+        //contactsAdapter.submitList(data)
     }
 
 
@@ -37,6 +68,7 @@ class ContactsListActivity : AppCompatActivity() {
 
     private fun initRecyclerView(){
         contactsAdapter = ContactListAdapter()
-        contactListRecycler?.adapter = contactsAdapter
+        TODO("contactListRecycler was the ID of the view that was in the activity. So modify the adapter accordingly")
+        //contactListRecycler?.adapter = contactsAdapter
     }
 }
