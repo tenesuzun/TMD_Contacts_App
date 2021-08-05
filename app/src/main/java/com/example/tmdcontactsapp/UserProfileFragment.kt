16fe9fe1e9dc9c0cc7eaf580681cc4cb2cacb878
tenesuzun <tahaenesuzun@gmail.com>
2021-08-05 14:54:13 +0300

    In user profile on update cancellation, texts return their original values

--- app/src/main/java/com/example/tmdcontactsapp/UserProfileFragment.kt
+++ app/src/main/java/com/example/tmdcontactsapp/UserProfileFragment.kt
@@ -27,6 +27,30 @@ private const val userArgToken = "token"
 class UserProfileFragment : Fragment() {
     private var userEmail: String? = null
     private var userToken: String? = null
+    private lateinit var profileFirstName: TextView
+    private lateinit var profileSurname: TextView
+    private lateinit var profileEmail: TextView
+    private lateinit var profilePhoneNumber: TextView
+    private lateinit var profileWorkNumber: TextView
+    private lateinit var profileHomeNumber: TextView
+    private lateinit var profileAddress: TextView
+    private lateinit var profileCompany: TextView
+    private lateinit var profileTitle: TextView
+    private lateinit var profileBirthday: TextView
+    private lateinit var profileNotes: TextView
+    private lateinit var profilePicture: ImageView
+    private lateinit var tempFirstName : String
+    private lateinit var tempSurname : String
+    private lateinit var tempEmail : String
+    private lateinit var tempPhoneNumber : String
+    private lateinit var tempHomeNumber : String
+    private lateinit var tempWorkNumber : String
+    private lateinit var tempAddress : String
+    private lateinit var tempCompany : String
+    private lateinit var tempTitle : String
+    private lateinit var tempBirthday : String
+    private lateinit var tempNotes : String
+    private lateinit var tempString: String
 
     fun newInstance(bundle: Bundle): UserProfileFragment {
         val fragment = UserProfileFragment()
@@ -51,21 +75,19 @@ class UserProfileFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_user_profile, container, false)
 
         //region View initializers
-        val profileFirstName: TextView = view.findViewById(R.id.userProfileFirstName)
-        val profileSurname: TextView = view.findViewById(R.id.userProfileSurname)
-        val profileEmail: TextView = view.findViewById(R.id.userProfileEmailAddress)
-        val profilePhoneNumber: TextView = view.findViewById(R.id.userProfilePhoneNumber)
-        val profileWorkNumber: TextView = view.findViewById(R.id.userProfileWorkPhone)
-        val profileHomeNumber: TextView = view.findViewById(R.id.userProfileHomePhone)
-        val profileAddress: TextView = view.findViewById(R.id.userProfileAddress)
-        val profileCompany: TextView = view.findViewById(R.id.userProfileCompany)
-        val profileTitle: TextView = view.findViewById(R.id.userProfileWorkTitle)
-        val profileBirthday: TextView = view.findViewById(R.id.userProfileBirthday)
-        val profileNotes: TextView = view.findViewById(R.id.userProfileNotes)
-        val profilePicture: ImageView = view.findViewById(R.id.userProfilePP)
+        profileFirstName= view.findViewById(R.id.userProfileFirstName)
+        profileSurname= view.findViewById(R.id.userProfileSurname)
+        profileEmail= view.findViewById(R.id.userProfileEmailAddress)
+        profilePhoneNumber= view.findViewById(R.id.userProfilePhoneNumber)
+        profileWorkNumber= view.findViewById(R.id.userProfileWorkPhone)
+        profileHomeNumber= view.findViewById(R.id.userProfileHomePhone)
+        profileAddress= view.findViewById(R.id.userProfileAddress)
+        profileCompany= view.findViewById(R.id.userProfileCompany)
+        profileTitle= view.findViewById(R.id.userProfileWorkTitle)
+        profileBirthday= view.findViewById(R.id.userProfileBirthday)
+        profileNotes= view.findViewById(R.id.userProfileNotes)
+        profilePicture= view.findViewById(R.id.userProfilePP)
         //endregion
-
-        var tempString: String
 
         val retrofit = Retrofit.Builder()
             .baseUrl("http://tmdcontacts-api.dev.tmd/api/")
@@ -78,17 +100,17 @@ class UserProfileFragment : Fragment() {
             override fun onResponse(call: Call<LoggedUserResponse>, response: Response<LoggedUserResponse>){
                 when(response.code()){
                     200 ->{
-                        profileFirstName.text = response.body()!!.name
-                        profileSurname.text = response.body()!!.surname
-                        profileEmail.text = response.body()!!.email
-                        profilePhoneNumber.text = response.body()!!.tel
-                        profileWorkNumber.text = response.body()!!.telBusiness
-                        profileHomeNumber.text = response.body()!!.telHome
-                        profileAddress.text = response.body()!!.address
-                        profileCompany.text = response.body()!!.company
-                        profileTitle.text = response.body()!!.title
-                        profileBirthday.text = response.body()!!.birthDate
-                        profileNotes.text = response.body()!!.note
+                        tempFirstName = response.body()!!.name
+                        tempSurname = response.body()!!.surname
+                        tempEmail = response.body()!!.email
+                        tempPhoneNumber = response.body()!!.tel
+                        tempHomeNumber = response.body()!!.telBusiness
+                        tempWorkNumber = response.body()!!.telHome
+                        tempAddress = response.body()!!.address
+                        tempCompany = response.body()!!.company
+                        tempTitle = response.body()!!.title
+                        tempBirthday = response.body()!!.birthDate
+                        tempNotes = response.body()!!.note
                         tempString = response.body()!!.photo
                         if(tempString == ""){
                             profilePicture.setImageResource(R.drawable.ic_round_account_box_24)
@@ -101,6 +123,17 @@ class UserProfileFragment : Fragment() {
                                     imageBytes.size
                                 ))
                         }
+                        profileFirstName.text = tempFirstName
+                        profileSurname.text = tempSurname
+                        profileEmail.text = tempEmail
+                        profilePhoneNumber.text = tempPhoneNumber
+                        profileWorkNumber.text = tempWorkNumber
+                        profileHomeNumber.text = tempHomeNumber
+                        profileAddress.text = tempAddress
+                        profileCompany.text = tempCompany
+                        profileTitle.text = tempTitle
+                        profileBirthday.text = tempBirthday
+                        profileNotes.text = tempNotes
                     }else -> {
                     Toast.makeText(context,"Internal Server error", Toast.LENGTH_SHORT).show()}
                 }
@@ -131,15 +164,16 @@ class UserProfileFragment : Fragment() {
         }
 
         userProfileCancelBtn.setOnClickListener{
-            makeViewsUnable()
+            cancelUpdate()
         }
     }
 
     private fun updateButtonClicked(){
-        //
+        //TODO Users must be able to update their information
+
     }
 
-    private fun makeViewsUnable(){
+    private fun cancelUpdate(){
         userProfileLogout.visibility = View.VISIBLE
         userProfileEditBtn.visibility = View.VISIBLE
         userProfileUpdate.visibility = View.GONE
@@ -156,6 +190,29 @@ class UserProfileFragment : Fragment() {
         userProfileWorkTitle.isEnabled = false
         userProfileBirthday.isEnabled = false
         userProfileNotes.isEnabled = false
+
+        userProfileFirstName.setText(tempFirstName)
+        userProfileSurname.setText(tempSurname)
+        userProfileEmailAddress.setText(tempEmail)
+        userProfilePhoneNumber.setText(tempPhoneNumber)
+        userProfileWorkPhone.setText(tempWorkNumber)
+        userProfileHomePhone.setText(tempHomeNumber)
+        userProfileAddress.setText(tempAddress)
+        userProfileCompany.setText(tempCompany)
+        userProfileWorkTitle.setText(tempTitle)
+        userProfileBirthday.setText(tempBirthday)
+        userProfileNotes.setText(tempNotes)
+        if(tempString == ""){
+            profilePicture.setImageResource(R.drawable.ic_round_account_box_24)
+        }else{
+            val imageBytes = Base64.decode(tempString,0)
+            profilePicture.setImageBitmap(
+                BitmapFactory.decodeByteArray(
+                    imageBytes,
+                    0,
+                    imageBytes.size
+                ))
+        }
     }
 
     private fun editButtonClicked() {
