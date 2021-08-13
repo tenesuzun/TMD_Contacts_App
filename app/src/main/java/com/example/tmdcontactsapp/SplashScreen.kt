0c61b61aae726fbfa0f9298347619d49679843d8
tenesuzun <tahaenesuzun@gmail.com>
2021-08-13 17:18:14 +0300

    animation

---
+++ app/src/main/java/com/example/tmdcontactsapp/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.tmdcontactsapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val slideAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        loadingLogo.startAnimation(slideAnimation)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
