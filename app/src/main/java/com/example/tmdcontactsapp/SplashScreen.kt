a5a51452efa296f74de17e95e618a4ee6f3f7c82
tenesuzun <tahaenesuzun@gmail.com>
2021-08-17 10:57:49 +0300

    View binding for splash screen

--- app/src/main/java/com/example/tmdcontactsapp/SplashScreen.kt
+++ app/src/main/java/com/example/tmdcontactsapp/SplashScreen.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import kotlinx.android.synthetic.main.activity_splash_screen.*
+import com.example.tmdcontactsapp.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
 
         val slideAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.side_slide)
-        loadingLogo.startAnimation(slideAnimation)
+        binding.loadingLogo.startAnimation(slideAnimation)
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
@@ -22,9 +23,10 @@ class SplashScreen : AppCompatActivity() {
             finish()
         }, 3000)
 
-        findViewById<ConstraintLayout>(R.id.splashScreen).setOnClickListener{
+        binding.splashScreen.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+        setContentView(binding.root)
     }
 }
